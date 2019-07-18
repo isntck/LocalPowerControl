b717751cb02d4875253a7a52b1b5da536ba18942
twoaten <twoaten@gmail.com>
2019-07-18 19:56:29 +0900

    .

--- LocalPowerControl_App/Src/main.c
+++ LocalPowerControl_App/Src/main.c
@@ -424,7 +424,7 @@ static void MX_TIM3_Init(void)
   htim3.Instance = TIM3;
   htim3.Init.Prescaler = 72;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 9;
+  htim3.Init.Period = 1;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
